fb2df99fb8e7ad0bad78ef82ee7ee715d4d90565
kayweeee <tankaywee09@gmail.com>
2024-04-19 23:23:25 +0800

    cleanup

--- models.py
+++ models.py
@@ -49,50 +49,6 @@ class CNN_LSTM(nn.Module):
         x = self.out(x)
         return x
 
-
-class SepCNN_LSTM(nn.Module):
-    def __init__(self, dimensions):
-        super(SepCNN_LSTM, self).__init__()
-        self.vision_model = nn.Sequential(
-            nn.Conv2d(dimensions[1], 64, kernel_size=(1, 2), padding='same'),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d((1, 2)),
-            nn.Flatten(),
-            nn.BatchNorm1d(32768)
-        )
-        self.encoded_frame_sequence = nn.Sequential(
-            nn.Linear(2 * 64 * (dimensions[2] // 2) * (dimensions[3] // 2), 608),
-            nn.ReLU(),
-            nn.BatchNorm1d(38)
-        )
-        self.encoded_video = nn.LSTM(608, 608, batch_first=True)
-        self.fc2 = nn.Sequential(
-            nn.Linear(608, 64),
-            nn.ReLU(),
-            nn.BatchNorm1d(64)
-        )
-        self.out = nn.Sequential(
-            nn.Dropout(0.5),
-            nn.Linear(64, 1),
-            nn.ReLU()
-        )
-
-    def forward(self, x):
-        batch_size, seq_len, channels, height, width = x.size()
-        x = x.view(batch_size * seq_len, channels, height, width)
-      
-        x = self.vision_model(x)
-        
-        x = x.view(batch_size, seq_len, -1)
-        
-        x = self.encoded_frame_sequence(x)
-        x, _ = self.encoded_video(x)
-
-        x = self.fc2(x[:, -1, :])
-        x = self.out(x)
-        return x
-
 # to be used in ConvGRU_LSTM
 class ConvGRU(nn.Module):
     def __init__(self, input_size, hidden_size, kernel_size):
